517d3188e72433bee96825b9e802055ca4fa1d21
David <89830181+kyotolean@users.noreply.github.com>
2023-05-14 04:16:16 +0300

    Delete task_2 directory

--- task_2/StringBuilder.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String numbers = "Hello ";
-        StringBuilder sb = new StringBuilder(numbers);
-        System.out.println(sb.insert(6, "World"));
-    }
-}
